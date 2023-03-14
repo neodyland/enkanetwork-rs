9fd73c6be6596443ddf77f4fb36876e844c229ad
Googlefan256 <yyykakeru@gmail.com>
2023-03-14 17:53:59 +0900

    style: 💄 format

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,10 +1,10 @@
-#[cfg(all(not(target_arch = "wasm32"),feature="async-io"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "async-io"))]
 pub mod io_async;
-#[cfg(any(target_arch = "wasm32",not(feature="async-io")))]
+#[cfg(any(target_arch = "wasm32", not(feature = "async-io")))]
 pub mod io_std;
-#[cfg(all(not(target_arch = "wasm32"),feature="async-io"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "async-io"))]
 pub use io_async::*;
-#[cfg(any(target_arch = "wasm32",not(feature="async-io")))]
+#[cfg(any(target_arch = "wasm32", not(feature = "async-io")))]
 pub use io_std::*;
 
 mod memory_cache;
